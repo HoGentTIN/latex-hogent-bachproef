aba86fad67595c044606ffd1e4c48c0361f983ad
Jan Willem <jan.willem@hogent.be>
2022-11-15 17:05:44 +0100

    Add linux scripts

---
+++ make_thesis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# utility batch file to render the tex file to a pdf
+
+# build the docker image with all tex dependencies
+docker build -t bpimg -f docker/Dockerfile .
+
+# build the proposal
+docker run --rm -v "$PWD":/bp bpimg env /bp/docker/render_thesis.sh bachproef *BP.tex
