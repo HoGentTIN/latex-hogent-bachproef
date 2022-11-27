a654e024343019b707c10fb3107f676a4491495b
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2022-11-27 16:03:12 +0100

    Scripts toegevoegd om de poster te compileren met Docker

---
+++ make_poster.bat
@@ -0,0 +1,10 @@
+@REM utility batch file to render the tex file to a pdf
+
+@REM build the docker image with all tex dependencies
+docker build -t bpimg -f docker/Dockerfile .
+
+@REM build the proposal
+docker run --rm -v %cd%:/bp bpimg sh /bp/docker/render_thesis.sh poster
+
+@REM keep window open after run
+pause
---
+++ make_poster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# utility batch file to render the tex file to a pdf
+
+set -o nounset
+set -o errexit
+
+# If Podman is installed (Fedora), use that instead of Docker
+if [ -x /usr/bin/podman ]; then
+  echo "Detected Podman"
+  container_cmd=/usr/bin/podman
+  # Without this mount option, the container will not have access to the
+  # shared volume /bp
+  mount_opts=":Z"
+else
+  echo "Using Docker"
+  container_cmd=/usr/bin/docker
+  mount_opts=
+fi
+
+# build the docker image with all tex dependencies
+${container_cmd} build --tag bpimg \
+  --file docker/Dockerfile .
+
+# build the proposal
+${container_cmd} run --rm \
+  --volume "$PWD":/bp"${mount_opts}" bpimg \
+  sh /bp/docker/render_thesis.sh poster
