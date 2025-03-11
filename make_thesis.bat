ecacc8a5eb1607829055ee122124178aa2746e16
Jaak Daemen <jaak.daemen@student.hogent.be>
2025-03-11 11:56:16 +0100

    Quotes toegevoegd aan .bat files

--- make_thesis.bat
+++ make_thesis.bat
@@ -4,7 +4,7 @@
 docker build -t bpimg -f docker/Dockerfile .
 
 @REM build the proposal
-docker run --rm -v %cd%:/bp bpimg sh /bp/docker/render_thesis.sh bachproef
+docker run --rm -v "%cd%:/bp" bpimg sh /bp/docker/render_thesis.sh bachproef
 
 @REM keep window open after run
 pause
